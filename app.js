411d254512012b301811481f160ace8153c5d9b5
Christian <chrisaug@rogers.com>
2020-01-04 17:09:20 -0500

    added favicon and mailchimp API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bp = require('body-parser');
-const jquery = require('jquery');
+const request = require('request');
 const port = 3000;
 const app = express();
 
@@ -12,7 +12,52 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/', function(req, res) {
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+    
+    var data = {
+        members: [
+            {
+                email_address: email, 
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    };
+
+    var jsondata = JSON.stringify(data);
+
+    var options = {
+        url: "https://us4.api.mailchimp.com/3.0/lists/e769dcb9ab",
+        method: "POST",
+        headers: {
+            "Authorization": "chris1 4802ac047d7cd08d55b55cf6eb3aa82d-us4"
+        },
+        body: jsondata
+    };
+
+    request(options, function(err, resp, body) {
+        if (err) {
+            res.sendFile(__dirname + '/failure.html');
+        } else {
+            if (res.statusCode === 200) {
+                res.sendFile(__dirname + '/success.html');
+            } else {
+                res.sendFile(__dirname + '/failure.html');
+            }
+        }
+    });
+});
+
 app.listen(port, function() {
     console.log('Server is running on port ' + port);
-    console.log(__dirname);
 });
+
+//e769dcb9ab -  list id
+
+//4802ac047d7cd08d55b55cf6eb3aa82d-us4 -api key
