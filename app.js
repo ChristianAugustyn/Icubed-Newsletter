43dffe0aa573b0925722c3654645d1ecbf7f3f1a
Christian <chrisaug@rogers.com>
2020-01-04 13:55:46 -0500

    signup page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bp = require('body-parser');
-
+const jquery = require('jquery');
+const port = 3000;
 const app = express();
 
 app.use(express.static('public'));
@@ -11,3 +12,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/signup.html');
 });
 
+app.listen(port, function() {
+    console.log('Server is running on port ' + port);
+    console.log(__dirname);
+});
